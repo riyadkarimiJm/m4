9f698ea5010cd56491ab34ba764f1bfc54508891
riyadkarimiJm <115711421+riyadkarimiJm@users.noreply.github.com>
2024-04-24 22:28:36 +0000

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 (function () {
 
-    var names = ["Yaakov", "John", "Jen", "Jason", "Paul", "Frank", "Larry", "Paula", "Laura", "Jim"];
+    var names = ["Yaakov", "John", "Jen", "Jason", "Paul", "Frank", "Larry", "Paulaa", "Laura", "Jim"];
     
     for (var i = 0; i < names.length; i++) {
     
